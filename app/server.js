8844e77baf702b43750e05a228c4b66ac803eac6
JerusalemBoy <jeffquater@gmail.com>
2026-03-17 16:01:55 +0530

    devops project

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
 res.writeHead(200, {'Content-Type':'text/plain'});
-res.end("DevOps CI/CD Pipeline Running!");
+res.end("DevOps CI/CD Pipeline Running! ---By Joel");
 });
 
 server.listen(3000);
